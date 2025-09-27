69cd5ff53e0e78ad007703708ad9e929641a8f19
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2025-09-27 22:17:42 +0545

    add events, tags and event_tags table

--- config/dbInit.js
+++ config/dbInit.js
@@ -18,6 +18,38 @@ async function init() {
       );
     `);
 
+    // Create Events table
+    await pool.query(`
+
+      CREATE TABLE events (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        title VARCHAR(100) NOT NULL,
+        description TEXT,
+        date_time TIMESTAMP NOT NULL,
+        location VARCHAR(100),
+        type VARCHAR(10) DEFAULT 'public',
+        user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+        created_at TIMESTAMP DEFAULT NOW()
+      );
+    `);
+
+    // Create Tags table
+    await pool.query(`
+      CREATE TABLE tags (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        name VARCHAR(50) UNIQUE
+      );
+    `);
+
+    // Create Event_Tags table
+    await pool.query(`
+      CREATE TABLE event_tags (
+        event_id UUID REFERENCES events(id) ON DELETE CASCADE,
+        tag_id UUID REFERENCES tags(id) ON DELETE CASCADE,
+        PRIMARY KEY(event_id, tag_id)
+      );
+    `);
+
     console.log("✅ Database initialized successfully");
   } catch (err) {
     console.error("❌ Error initializing database:", err.message);
